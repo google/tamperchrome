3f9dd51b22223d22ac42b6f4632a8f56df28cc29
Eduardo Vela <sirdarckcat@gmail.com>
2019-03-04 00:31:51 +0000

    Add UI for tamperchrome using Angular.io + Material.

---
+++ v2/ui/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material';
+import { MatChipsModule, MAT_CHIPS_DEFAULT_OPTIONS } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
+@NgModule({
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		MatButtonModule,
+		MatCheckboxModule,
+		ReactiveFormsModule,
+		FormsModule,
+		MatAutocompleteModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatChipsModule,
+		MatIconModule,
+		MatSlideToggleModule,
+		FlexLayoutModule
+	],
+	providers: [{
+		provide: MAT_CHIPS_DEFAULT_OPTIONS,
+		useValue: {
+			separatorKeyCodes: [ENTER, COMMA]
+		}
+	}],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
