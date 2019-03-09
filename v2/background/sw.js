60746eca76e3bfbb17a1684370deac39a19aed1d
Eduardo Vela <sirdarckcat@gmail.com>
2019-03-09 05:40:50 +0000

    Change folder structure.

--- v2/background/sw.js
+++ v2/background/sw.js
@@ -1,7 +1,7 @@
 oninstall = e => { e.waitUntil(self.skipWaiting()); };
 onactivate = e => { e.waitUntil(self.clients.claim()); };
 onfetch = e => {
-	// Add .js to all requested modules.
+	// Add .js to all requested modules from background page.
 	if (e.request.destination == 'script' && e.request.url.match(/\/\w+$/)) {
 		return e.respondWith(fetch(`${e.request.url}.js`));
 	}
