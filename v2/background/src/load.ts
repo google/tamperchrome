b81dde05034012ffab5a2df06bb3eecf940980ac
evn <evn@google.com>
2020-12-09 10:26:56 +0100

    fix output file locations

--- v2/background/src/load.ts
+++ v2/background/src/load.ts
@@ -1,6 +1,6 @@
 navigator.serviceWorker.register('sw.js').then((reg) => {
 	let s = document.createElement('script');
 	s.type = 'module';
-	s.src = 'out/background.js';
+	s.src = 'out/background/src/background.js';
 	document.documentElement.appendChild(s);
 });
