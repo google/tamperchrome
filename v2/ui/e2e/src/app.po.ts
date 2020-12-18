6cfb51380b866227c3e24c8640494d6c83de4f8f
evn <evn@google.com>
2020-12-18 11:37:10 +0100

    a11y tree with chrome debugger api

--- v2/ui/e2e/src/app.po.ts
+++ v2/ui/e2e/src/app.po.ts
@@ -1,32 +1,53 @@
 import { browser, by, element } from 'protractor';
 
-/*
- * Titus van Rijn was the fourth and only surviving child of Rembrandt.
- */
-function Titus(node) {
-  if (node) {
-      let attrs = [];
-      for(var key in node) {
-          if (['string', 'boolean'].includes(typeof node[key])) {
-              if (node[key] && key != "role" && (key != "checked" && node[key] != "none")) {
-                  attrs.push(key+(node[key].length?'='+node[key]:''));
-              }
-          }
-      }
-      let children = [];
-      for(let child = node.firstChild; child; child = child.nextSibling) {
-          children.push(Titus(child));
-      }
-      return {role: node.role, attrs, children};
-  }
-}
-
 export class AppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+  async navigateTo() {
+    await browser.driver.sendChromiumCommand('Accessibility.enable', {});
+    return browser.get(browser.baseUrl);
   }
 
-  takeSnapshot() {
-    return browser.executeScript(`${Titus}; return Titus(await getComputedAccessibleNode(document.body))`) as Promise<string>;
+  async takeDevtoolsSnapshot() {
+    return browser.driver.sendChromiumCommandAndGetResult('Accessibility.getFullAXTree', {});
+  }
+
+  toTree(devToolsSnapshot) {
+    const leaves = new Map;
+    for(const node of devToolsSnapshot.nodes) {
+      leaves.set(node.nodeId, {});
+    }
+    for(const node of devToolsSnapshot.nodes) {
+      const leaf = leaves.get(node.nodeId);
+      leaf.ignored = node.ignored;
+      leaf.role = node.role?.value;
+      leaf.attrs = {
+        name: node.name?.value || undefined,
+        description: node.description?.value || undefined,
+        value: node.value?.value || undefined
+      };
+      leaf.children = node.childIds.map(id=>leaves.get(id));
+      leaf.props = node.properties.filter(prop=>prop.value?.value).map(prop=>`${prop.name}=${prop.value.value}`);
+    }
+    return leaves.get("1");
+  }
+
+  serializeTree(tree, spaces = 0) {
+    if (tree.ignored || tree.props.includes('hidden=true')) return '';
+    const head = `${
+      tree.role
+    }${
+      JSON.stringify(tree.props)
+    }${
+      JSON.stringify(tree.attrs)
+    }`;
+    const body = tree.children.map(child=>this.serializeTree(child, spaces + 1)).join('');
+    return `${
+      head != 'generic[]{}' ? `${' '.repeat(spaces)}${head}\n` : ''
+    }${
+      body
+    }`;
+  }
+
+  async takeSnapshot() {
+    return this.serializeTree(this.toTree(await this.takeDevtoolsSnapshot()));
   }
 }
