afdc2590d47aa0fbd6703222991c352040f3224e
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2020-12-31 11:36:19 +0100

    wait for service worker to load- (#126)

--- v2/background/src/load.ts
+++ v2/background/src/load.ts
@@ -1,6 +1,8 @@
-navigator.serviceWorker.register('sw.js').then((reg) => {
+(async ()=>{
+	const reg = await navigator.serviceWorker.register('sw.js');
+	await navigator.serviceWorker.ready;
 	let s = document.createElement('script');
 	s.type = 'module';
 	s.src = 'out/background/src/background.js';
 	document.documentElement.appendChild(s);
-});
+})();
