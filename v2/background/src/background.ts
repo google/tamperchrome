685eda7122c2d836c9401b1c22e6ff86ea1e9ca7
Minzhi Liao <liao02x@gmail.com>
2022-05-05 22:53:57 -0700

    port to mv3

--- v2/background/src/background.ts
+++ v2/background/src/background.ts
@@ -2,49 +2,70 @@ import { Debuggee } from "./debuggee";
 import { Interception } from "./interception";
 import { Intercepted } from "./request";
 
-chrome.browserAction.onClicked.addListener(async (tab: chrome.tabs.Tab) => {
+const openAsync = (config: object): Promise<chrome.windows.Window | undefined> => {
+  return new Promise((resolve, reject) => {
+    chrome.windows.create(config, (window) => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError.message);
+      }
+      resolve(window);
+    })
+  })
+}
+
+chrome.action.onClicked.addListener(async (tab: chrome.tabs.Tab) => {
   let dbg: Debuggee = new Debuggee(tab);
   try {
     await dbg.attach();
   } catch(e) {
-    console.error(e);
-    alert('Failed to attach to ' + tab.url + ' - probably because of enterprise policies.');
+    console.error(e, JSON.stringify(e));
+    // alert is not supported
+    // alert('Failed to attach to ' + tab.url + ' - probably because of enterprise policies.');
     return;
   }
-  let int: Interception = Interception.build(dbg);
-  const popup = open('/ui/dist/ui/index.html', `tamperchrome_${tab.id}`, 'menubar=0,innerWidth=900,innerHeight=800');
+  const int: Interception = Interception.build(dbg);
+
+  const popup = await openAsync({
+    url: '/ui/dist/ui/index.html',
+    type: 'popup',
+    width: 900,
+    height: 800,
+  });
+
   if (!popup) {
     throw new Error('Failed to open UI window');
   }
-  popup.onmessage = async e => {
-    if (e.data.event == 'capture') {
-      await int.capture(e.data.pattern || '*');
-      await int.onRequest(async (req: Intercepted) => {
-        const mc = new MessageChannel;
-        popup.postMessage({'event': 'onRequest', request: JSON.parse(JSON.stringify(req))}, origin, [mc.port1]);
-        mc.port2.onmessage = async (e) => {
-          await req.continueRequest(e.data.request);
-        };
-      });
-      await int.onResponse(async (res: Intercepted) => {
-        const mc = new MessageChannel;
-        const bodyMc = new MessageChannel;
-        popup.postMessage({'event': 'onResponse', response: JSON.parse(JSON.stringify(res))}, origin, [mc.port1, bodyMc.port1]);
-        mc.port2.onmessage = async (e) => {
-          await res.continueResponse(e.data.response);
-        };
-        bodyMc.port2.onmessage = async (e) => {
-          bodyMc.port2.postMessage(await res.getResponseBody());
-        };
-      });
-    } else if (e.data.event == 'reloadTab') {
-      chrome.tabs.reload(tab.id!, {bypassCache: true});
-    }
-  };
-  popup.onload = () => popup.onunload = () => {
-    dbg.detach();
-    popup.close();
-  }
+
+  chrome.runtime.onConnect.addListener(port => {
+    if (port.name !== 'tamperchrome') return
+    port.onMessage.addListener(async (msg) => {
+      if (msg.name === "capture") {
+        await int.capture(msg.data.pattern || '*');
+        await int.onRequest(async (req: Intercepted) => {
+          port.postMessage({ name: 'onRequest', data: { request: req } });
+          port.onMessage.addListener(async (msg) => {
+            if (msg.name === req.id) await req.continueRequest(msg.request);
+          })
+        });
+        await int.onResponse(async (res: Intercepted) => {
+          port.postMessage({ name: 'onResponse', data: { response: res } });
+          port.onMessage.addListener(async (msg) => {
+            if (msg.name === res.id + '-res') await res.continueResponse(msg.response);
+          })
+          port.onMessage.addListener(async (msg) => {
+            if (msg.name === res.id + '-body') {
+              const data = await res.getResponseBody()
+              port.postMessage({ name: res.id + '-body', data })
+            }
+          })
+        });
+      } else if (msg.name === "reloadTab") {
+        chrome.tabs.reload(tab.id!, { bypassCache: true });
+      }
+    });
+    port.onDisconnect.addListener(() => dbg.detach())
+  });
+
   await dbg.waitForDetach();
-  popup.close();
+  await chrome.windows.remove(popup.id!);
 });
--- v2/background/src/load.ts
+++
@@ -1,8 +0,0 @@
-(async ()=>{
-	const reg = await navigator.serviceWorker.register('sw.js');
-	await navigator.serviceWorker.ready;
-	let s = document.createElement('script');
-	s.type = 'module';
-	s.src = 'out/background/src/background.js';
-	document.documentElement.appendChild(s);
-})();
